ee67abcd0ef5563ba02e4f5b764a4ecf61e4c747
JozefGlu <40337144+JozefGlu@users.noreply.github.com>
2023-03-21 01:16:02 +0100

    darkmode

--- JS/index.js
+++ JS/index.js
@@ -1,6 +1,12 @@
 const navToggle = document.querySelector('#hamburger-menu');
 const themeSwitch = document.querySelector('#theme-switch');
+const navMenu = document.querySelectorAll('.nav__menu-link');
+const navMenuItems = document.querySelectorAll('.nav__menu-item');
 
+console.log('navMenu', navMenu);
+
+const arrayFromNodeList = (list) => Array.from(list);
+console.log('arrayFromNodeList', arrayFromNodeList(navMenu));
 
 const handleClickNavToggle = ({ currentTarget: target }) => {
     const list = document.querySelector(`#${target.getAttribute('aria-controls')}`);
@@ -10,6 +16,10 @@ const handleClickNavToggle = ({ currentTarget: target }) => {
         list.classList.remove('nav__menu-list--open');
         target.setAttribute('aria-expanded', 'false');
         target.parentNode.classList.remove('nav--open');
+
+        arrayFromNodeList(navMenu).forEach((current) => current.classList.remove('nav__menu--ramification'));
+        arrayFromNodeList(navMenuItems).forEach((current) => current.classList.remove('nav__menu-item--reduction'));
+
 
     } else {
         list.classList.add('nav__menu-list--open');
@@ -22,7 +32,13 @@ const handleClickNavToggle = ({ currentTarget: target }) => {
 const handleClickThemeSwitch = () => {
     document.documentElement.classList.toggle('darkmode');
 }
+const handleClickClasslist = () => {
+    arrayFromNodeList(navMenu).forEach((current) => current.classList.toggle('nav__menu--ramification'));
+    arrayFromNodeList(navMenuItems).forEach((current) => current.classList.toggle('nav__menu-item--reduction'));
+}
 
+
+arrayFromNodeList(navMenu).forEach((item) => item.addEventListener('click', handleClickClasslist));
 navToggle.addEventListener('click', handleClickNavToggle);
 themeSwitch.addEventListener('click', handleClickThemeSwitch);
 
